3ae20f85dae9ad8c7982620520af231a448eb4cc
Tilda Eriksson Lindgren <tilda.e.l.1997@gmail.com>
2018-11-14 11:52:36 +0100

    htfvhgv
    hgvhgv

--- koma-app/src/App.js
+++ koma-app/src/App.js
@@ -7,8 +7,9 @@ class App extends Component {
     return (
       <div className="Soooop">
         <h1 className="App"> 
-        HEJ LOSERS, I LOVE YOU ALL
+        Slingerling AB
         </h1>
+        <h3 className="App"> Det här kan vi göra för att hjälpa dig! </h3>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
@@ -17,11 +18,11 @@ class App extends Component {
           </p>
           <a
             className="Tryck INTE på MIG om du är FEG"
-            href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+            href="https://www.youtube.com/watch?v=c3c3UTYNZ6Q"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Tryck INTE på MIG om du är FEG
+            Tryck här för det vi kan göra för dig
           </a>
         </header>
       </div>

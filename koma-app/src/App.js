c2411572114915b5c94788f1a048c62cdc84d5af
Hanna <timander.hanna@gmail.com>
2018-11-14 11:01:29 +0100

    Länk igen
    Uppdaterade länktext

--- koma-app/src/App.js
+++ koma-app/src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Tryck INTE på MIG om du är FEG
           </a>
         </header>
       </div>

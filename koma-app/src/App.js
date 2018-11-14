2d27a28c5e02667840673dca45f79ebfa0d2a50c
Hanna <timander.hanna@gmail.com>
2018-11-14 10:59:43 +0100

    Woopidoh
    Hanna säger hej

--- koma-app/src/App.js
+++ koma-app/src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Hejsan hoppsan KOMPISAR
+            Hejsan hoppsan KOMPISAr säger hanna
           </p>
           <a
             className="App-link"

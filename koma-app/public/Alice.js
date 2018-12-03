9de652eb4229c7d65f9c8f1868d760272c6603bb
Alice <alicedromberg@gmail.com>
2018-12-03 14:33:00 +0100

    Meny
    klar med en version av menyn(den är okej i guess)

--- koma-app/public/Alice.js
+++ koma-app/public/Alice.js
@@ -1,24 +1,38 @@
-/*function(){
-if ($("meny").style.display = "inline-block"){
-	$(window).click(function() {
-		document.getElementById("meny").style.display = "none";
-	});
-}
-}	*/
+$(function() {
+  $("body").click(function(e) {
+    if (e.target.id == "menydiv" || $(e.target).parents("#menydiv").length) {
+      $(document).ready(function(){
+			$('#menydiv').css("height", "520px");
+			$('#hem').fadeIn(500);
+			$('#bok').delay(100).fadeIn(500);
+			$('#profil').delay(200).fadeIn(500);
+			$('#kalender').delay(300).fadeIn(500);
+			
 
-$(document).ready(function(){
-	$('#menydiv').click(function(){
-		$('#menydiv').css("height", "400px")
+			
 		});
-		//$('#meny').slideDown(500);
+    } else {
+      $(document).ready(function(){
+		$('#menydiv').css("height", "90px");
+		$('#kalender').fadeOut(300);
+		$('#profil').delay(50).fadeOut(300);
+		$('#bok').delay(150).fadeOut(300);
+		$('#hem').delay(250).fadeOut(300);
+	
+		
+		
 	});
+    }
+  });
+});
 
 $(document).mouseup(function(e)
 {
 	var container = $('#menydiv');
 
-	if(!container.is(e.target) && container.has(e.target).lenght === 0)
+	if(container.is(e.target))
 	{
 		display("hej");
 	}
-})
+});
+

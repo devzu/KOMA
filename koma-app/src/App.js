c48e8233c2c377629ae06859486f591080f135d4
Azudator <lindqvist004@gmail.com>
2018-12-08 13:06:53 +0100

    Prova att skicka från VSC

--- koma-app/src/App.js
+++ koma-app/src/App.js
@@ -1,31 +1,3 @@
-
-//import React, { Component } from 'react';
-//import logo from './logo.svg';
-//import './App.css';
-//
-//class App extends Component {
-//  render() {
-//    return (
-//      <div className="Soooop">
-//        <h1 className="App"> 
-//        HEJ LOSERS, I LOVE YOU ALL
-//        </h1>
-//        <header className="App-header">
-//          <img src={logo} className="App-logo" alt="logo" />
-//          <p>
-//            Hejsan kul dhsjvv
-//
-//          </p>
-//          <a
-//            className="Tryck INTE på MIG om du är FEG"
-//            href="https://www.youtube.com/watch?v=dQw4w9WgXcQ"
-//            target="_blank"
-//            rel="noopener noreferrer"
-//          >
-//            Tryck INTE på MIG om du är FEG
-//          </a>
-//        </header>
-
 // **----- DEL 1 -----**
 //IMPORTERA VIKTIGA SAKER
 

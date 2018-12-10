f31d7083a2e6b4568685c0daab7854236cee5abe
Azudator <lindqvist004@gmail.com>
2018-12-10 15:01:58 +0100

    STÖN
    SUCK

--- koma-app/src/calendar.js
+++ koma-app/src/calendar.js
@@ -1,101 +1,34 @@
 import React, { Component } from 'react';
 import './main.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import {Container, Row, Col} from 'reactstrap';
 import TipsPage from './tips';
 
 const CalendarPage = () => ( 
- <div className="content-box">
- <h1 className="page-title">Kalendar</h1>
-        <div className="mittenBox"> <TipsContent/>
-        </div>
-  
+ <div className="">
+ <Container><Row><Col s="6">fuck</Col><Col s="4">THIS</Col></Row></Container>
 </div>
 
 );
 
-class Popup extends React.Component{
+class Kalender extends React.Component{
+        render(){
+                return(
+                        <div className="NOPE">lol</div>
+                )
+        }
+}
+
+
+class Deadlines extends React.Component{
+        render(){
+                return(
+                        <div className="NOPE">
+                                <div className="ThisWeek"><h3>Denna vecka:</h3></div>
+                        </div>
+                )
+        }
+}
     
-        // Initial states of Popup:
-        // Nollan counter, inga textfält (goalFields)
-        state = {
-            counter: 0,
-            enableButton: true,
-            goalFields: []
-        };
-        
-        // Arrow function
-        addGoal = () => {
-            // Plussa på countern
-            this.setState({ 
-                counter: this.state.counter +  1,
-                enableButton: false
-            });
-            // Kalla på createField
-            this.createField(this);
-            
-        } 
-        
-        render() {
-        {/**/}
-            return (
-              <div className='popup'>
-                <div className='popup_inner'>
-    
-                    <h1>Här finns massa text</h1>
-    
-                    
-    
-                    <button 
-                    onClick={this.props.closePopup}>KLAR</button>
-                
-                </div>
-              </div>
-    
-            );
-      }
-     
-    
-    }
-    
-    
-    class TipsContent extends React.Component {
-        
-      constructor() {
-        super();
-        this.state = {
-          showPopup: false
-        };
-      }
-        
-      togglePopup() {
-        this.setState({
-          showPopup: !this.state.showPopup
-        });
-      }
-        
-      render() {
-        return (
-          <div>
-            
-            <h1>Nejmen</h1>
-            
-            <button onClick={this.togglePopup.bind(this)}>
-                En knapp
-            </button>
-            
-            {/* Se detta som en if-sats:
-            // om showPopup == true, gör det som syns: 
-            // Annars, gör ingenting (null)*/}
-            {this.state.showPopup ? 
-              <Popup
-                closePopup={this.togglePopup.bind(this)} 
-              />
-              : null
-            }
-    
-          </div>
-        );
-      }
-    }
     
 export default CalendarPage;

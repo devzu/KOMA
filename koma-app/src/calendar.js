de9a068f3eea4733ce12e8c7c2f20d82b4abdedf
Azudator <lindqvist004@gmail.com>
2018-12-10 16:25:23 +0100

    Calender lol
    dskjfn

--- koma-app/src/calendar.js
+++ koma-app/src/calendar.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react';
 import './main.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import {Container, Row, Col} from 'reactstrap';
+import Calendar from 'react-calendar';
 import TipsPage from './tips';
 
 const CalendarPage = () => ( 
  <div className="">
- <Container><Row><Col s="6">fuck</Col><Col s="4">THIS</Col></Row></Container>
+ <Container><Row><Col xl="3"><div class="Blue"><Deadlines/></div></Col><Col xl="6"><div class="Blue"><Kalender/></div></Col></Row></Container>
 </div>
 
 );
@@ -14,7 +15,8 @@ const CalendarPage = () => (
 class Kalender extends React.Component{
         render(){
                 return(
-                        <div className="NOPE">lol</div>
+                        <div className="NOPE"><h3>HELA JÄVLA MÅNAN'</h3>
+                        <Calendar/></div>
                 )
         }
 }
@@ -24,7 +26,19 @@ class Deadlines extends React.Component{
         render(){
                 return(
                         <div className="NOPE">
-                                <div className="ThisWeek"><h3>Denna vecka:</h3></div>
+                                <div className="ThisWeek"><h3>Denna vecka:</h3>
+                                <hr></hr>
+                                <p><span className="glyphicon glyphicon-calendar liten"></span> - <strike>(11/12) En de adline</strike></p>
+                                <p><span className="glyphicon glyphicon-calendar liten reddo"></span> - (18/12)En deadline till</p> 
+                                <br></br><br></br>
+                                <h3>Denna månaden:</h3>
+                                <hr></hr>
+                                <p><span className="glyphicon glyphicon-calendar liten"></span> - Massa inlämning</p>
+                                <p><span className="glyphicon glyphicon-calendar liten"></span> - So tenta</p>
+                                <p><span className="glyphicon glyphicon-calendar liten"></span> - Much wow</p><br></br>
+                             
+
+                                </div>
                         </div>
                 )
         }

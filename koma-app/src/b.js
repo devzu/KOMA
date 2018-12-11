04cdc6dba2e8a9723ad78edeea390721b4d8f943
Azudator <lindqvist004@gmail.com>
2018-12-11 13:01:03 +0100

    Update koma-app/src/b.js

--- koma-app/src/b.js
+++ koma-app/src/b.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import './main.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import ReactDOM from 'react-dom';
 
 const HelloWorldText = () => ( 
  <div className="containWithStuff">jhbjhbj
-        <div className="">
+        <div className="" id="root">
 fuck it all magna melius eos eu. Et mea lobortis suscipiantur, ignota consequat conceptam sit ei. Vidit omnes his et. Cu ferri euismod corpora cum. </div>
 
 <App/>
@@ -17,7 +18,9 @@ fuck it all magna melius eos eu. Et mea lobortis suscipiantur, ignota consequat 
 class App extends Component {
         constructor() {
             super();
+            this.handleClick1 = this.handleClick1.bind(this);
             this.state = {
+                    
                 viewContent1: false,
                 viewContent2: false,
                 viewContent3: false,
@@ -26,7 +29,7 @@ class App extends Component {
             }
         }
     
-        handleClick1 = (button) => {
+        handleClick1 = () => {
             this.setState({ viewContent1: true, viewContent2: false, viewContent3: false, viewContent4: false })
         }
 
@@ -42,26 +45,60 @@ class App extends Component {
                 this.setState({ viewContent1: false, viewContent2: false, viewContent3: false, viewContent4: true  })
             }
         
-        
     
         render() {
-            if(this.state.viewContent1 === true) return  (
-                <div > 
-                        DU TRYCKTE PÅ KNAPP 1!!!
-                <button className='button button1' onClick={() => this.handleClick1}> BUTTON 1</button>
-                <button className='button button2' onClick={() => this.handleClick2}> BUTTON 2</button>
-                <button className='button button3' onClick={() => this.handleClick3}> BUTTON 3</button>
-                <button className='button button3' onClick={() => this.handleClick4}> BUTTON 4</button>
+            const click1= this.state.viewContent1;
+            const click2= this.state.viewContent2;
+            const click3= this.state.viewContent3;
+            const click4= this.state.viewContent4;
+            if(click1) return  (
+                <div id=""> 
+                        
+                <button className='button button1' onClick={this.handleClick1}>VIDEO</button>
+                <button className='button button2' onClick={this.handleClick2}> HEMSIDA</button>
+                <button className='button button3' onClick={this.handleClick3}> BÖCKER</button>
+                <button className='button button3' onClick={this.handleClick4}> APPAR</button>
                 AAJNSJHSNJHDBDJH
                 </div>)
-        else return (<div>
-        <button className='button button1' onClick={() => this.handleClick1}> BUTTON 1</button>
-        {/* <button className='button button2' onClick={() => this.handleClick2}> BUTTON 2</button>
-        <button className='button button3' onClick={() => this.handleClick3}> BUTTON 3</button>
-        <button className='button button3' onClick={() => this.handleClick4}> BUTTON 4</button> */}
+                else if (click2) return ( 
+                        <div id=""> 
+                        
+                        <button className='button button1' onClick={this.handleClick1}>VIDEO</button>
+                <button className='button button2' onClick={this.handleClick2}> HEMSIDA</button>
+                <button className='button button3' onClick={this.handleClick3}> BÖCKER</button>
+                <button className='button button3' onClick={this.handleClick4}> APPAR</button>
+                AAJNSJHSNJHDBDJH
+                </div>)
+                  else if (click3) return ( 
+                        <div id=""> 
+                        
+                        <button className='button button1' onClick={this.handleClick1}>VIDEO</button>
+                <button className='button button2' onClick={this.handleClick2}> HEMSIDA</button>
+                <button className='button button3' onClick={this.handleClick3}> BÖCKER</button>
+                <button className='button button3' onClick={this.handleClick4}> APPAR</button>
+                blubb
+                </div>)
+                  else if (click4) return ( 
+                        <div id=""> 
+                        
+                        <button className='button button1' onClick={this.handleClick1}>VIDEO</button>
+                <button className='button button2' onClick={this.handleClick2}> HEMSIDA</button>
+                <button className='button button3' onClick={this.handleClick3}> BÖCKER</button>
+                <button className='button button3' onClick={this.handleClick4}> APPAR</button>
+                
+                </div>)
+
+        else return (<div id="">
+        <button className='button button1' onClick={this.handleClick1}>VIDEO</button>
+                <button className='button button2' onClick={this.handleClick2}> HEMSIDA</button>
+                <button className='button button3' onClick={this.handleClick3}> BÖCKER</button>
+                <button className='button button3' onClick={this.handleClick4}> APPAR</button>
         <br/>okej försök trycka nu då
         </div>)
         }
+
+        
 }
+ReactDOM.render(<App />, document.getElementById('root'));
 
 export default HelloWorldText;

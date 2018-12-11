17f65aac0dfd363691a73b189b07ab75ba5a4cd8
Elin <elliiiin864@gmail.com>
2018-12-11 11:58:54 +0100

    länkar klara

--- koma-app/src/App.js
+++ koma-app/src/App.js
@@ -12,17 +12,6 @@
  import TipsPage from './tips.js';
  import HelloWorldText from './b.js';
  import './main.css'
-import React, { Component } from 'react';
-import logo from './image/logo.svg';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import MainPage from './mainpage.js';
-import CalendarPage from './calendar.js';
-import ProfilePage from './profile.js';
-import TipsPage from './tips.js';
-import HelloWorldText from './b.js';
-import './main.css'
 
 
 // **--DEL TVÅ--**
@@ -130,86 +119,4 @@ const MainMenu = () => {
     }
 
     export default App;
- return (
-   
-   <div className="navbar">
-          
-     <div className="row"><Link to="/">
-     <button className="menubutton dissapear col"><span className="glyphicon glyphicon-stats dissapear"></span></button>
-     </Link></div> 
-     
-     <div className="row"><Link to="/knapp2">
-     <button className="menubutton dissapear col"><span className="glyphicon glyphicon-calendar dissapear"></span></button>
-     </Link></div>
-     
-     <div className="row">
-     <Link to="/knapp3">
-     <button className="menubutton dissapear col"><span className="glyphicon glyphicon-education dissapear"></span></button>
-     </Link></div>
-
-   </div>
- );
-};
-
-   class App extends Component 
-   {
-     render() 
-     {
-       return (
-           <Router>
-             <div> {/* OUTERMOST DIV INSIDE ROUTER */}
-
-             {/* DIV ROW FOR BOOSTRAP COL */}
-           <div className="row">
-
-             <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"></link>
-           {/* APP-CLASSNAME START */}
-             <div className="App col-s-12">
-
-             {/* ////PART ONE//// */}
-              {/* HEADER CLASSNAME START */}
-               <div className="App-header col-xl-1">
-
-                 {/* LOGO */}
-                   <Link to="/TestSida"><div className="Bild"><img src={logo} className="App-logo" alt="logo" /></div></Link>
-                 {/* END LOGO */}
-
-                 {/* MAINMENU FUNCTION */}
-                   <MainMenu />
-                 {/* END MAINMENU FUNCTION */}
-
-               </div>
-               {/* HEADER CLASSNAME END */}
-
-               {/* ////PART TWO//// */}
-               {/* CONTENT  */}
-               <div className="ContentanAvTentan col-s-11">
-                 <Route exact path="/" component={Startsida} />
-                 <Route exact path="/knapp2" component={Knapp2} />
-                 <Route exact path="/knapp3" component={Knapp3} />
-                 <Route exact path="/TestSida" component={TestSida} />
-                 {/* END CONTENT */}
-             
-               </div>
-
-             </div>{/* END APP-CLASSNAME START */}
-
-           
-             {/* END ROW, END CONTAINER */}
-           </div>
-
-           {/* OCEAN WAVE */}
-           <div className="app-wave"><div className="ocean">
-             <div className="wave"></div>
-             <div className="wave"></div></div></div> 
-             {/* OCEAN WAVE END*/}
-
-           {/* END OUTERMOST DIV CONTAINER BEFORE ROUTER */}
-           </div>
-           </Router>
-       );
-     }
-   }
-
-   export default App;
-
+ 
--- koma-app/src/tips.js
+++ koma-app/src/tips.js
@@ -8,177 +8,6 @@ const TipsPage = () => (
 );
 
 
-class PopupVideo extends React.Component{
-    
-    // Initial states of Popup:
-    // Nollan counter, inga textfält (goalFields)
-    state = {
-        counter: 0,
-        enableButton: true,
-        goalFields: []
-    };
-    
-    // Arrow function
-    addGoal = () => {
-        // Plussa på countern
-        this.setState({ 
-            counter: this.state.counter +  1,
-            enableButton: false
-        });
-        // Kalla på createField
-        this.createField(this);
-        
-    } 
-    
-    render() {
-    {/**/}
-        return (
-          <div className='popup'>
-            <div className='popup_inner'>
-
-                 {/* CONTAINMENT WITH VIDEOS AND STUFF */}
-    <div className="videosContainer">
-        <div className="VideoBox row">  <div className="col">
-        <iframe title="fuckIt" className="WhatEverBro" width="560" height="315" src="https://www.youtube.com/embed/aVS4W7GZSq0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
-        <div className="VideoDescText col"><h3>Lorem Ipsum</h3><p>Väldigt informativ video om Lorem Ipsum</p></div> </div>
-    </div>
-
-                
-
-                <button 
-                onClick={this.props.closePopup}>KLAR</button>
-            
-            </div>
-          </div>
-
-        );
-  }
- 
-
-}
-
-class PopupProd extends React.Component{
-    
-    // Initial states of Popup:
-    // Nollan counter, inga textfält (goalFields)
-    state = {
-        counter: 0,
-        enableButton: true,
-        goalFields: []
-    };
-    
-    // Arrow function
-    addGoal = () => {
-        // Plussa på countern
-        this.setState({ 
-            counter: this.state.counter +  1,
-            enableButton: false
-        });
-        // Kalla på crateField
-        this.createField(this);
-        
-    } 
-    
-    render() {
-    {/**/}
-        return (
-          <div className='popup'>
-            <div className='popup_inner'>
-
-                 {/* CONTAINMENT WITH VIDEOS AND STUFF */}
-    <div className="videosContainer">
-        ROLL THE DICE
-    </div>
-
-                
-
-                <button 
-                onClick={this.props.closePopup}>KLAR</button>
-            
-            </div>
-          </div>
-
-        );
-  }
- 
-
-}
-
-
-class VideoContent extends React.Component {
-    
-  constructor() {
-    super();
-    this.state = {
-      showPopup: false
-    };
-  }
-    
-  togglePopup() {
-    this.setState({
-      showPopup: !this.state.showPopup
-    });
-  }
-    
-  render() {
-    return (
-      <div className="col">
-        <button onClick={this.togglePopup.bind(this)} className="button">
-            En knapp
-        </button>
-        
-        {/* Se detta som en if-sats:
-        // om showPopup == true, gör det som syns: 
-        // Annars, gör ingenting (null)*/}
-        {this.state.showPopup ? 
-          <PopupVideo
-            closePopup={this.togglePopup.bind(this)} 
-          />
-          : null
-        }
-
-      </div>
-    );
-  }
-}
-
-
-class ProdContent extends React.Component {
-    
-  constructor() {
-    super();
-    this.state = {
-      showPopup: false
-    };
-  }
-    
-  togglePopup() {
-    this.setState({
-      showPopup: !this.state.showPopup
-    });
-  }
-    
-  render() {
-    return (
-      <div className="col">
-        <button onClick={this.togglePopup.bind(this)} className="button">
-            Två knapp
-        </button>
-        
-        {/* Se detta som en if-sats:
-        // om showPopup == true, gör det som syns: 
-        // Annars, gör ingenting (null)*/}
-        {this.state.showPopup ? 
-          <PopupProd
-            closePopup={this.togglePopup.bind(this)} 
-          />
-          : null
-        }
-
-      </div>
-    );
-  }
-}
 
 class Tips extends React.Component{
     
@@ -192,48 +21,56 @@ class Tips extends React.Component{
      No worries. Vi har samlat lite tips och tricks på studieteknik, som kanske kan hjälpa dig hålla fokus. 
      
  </p>
-
-    {/* ROW MENU BUTTONS WOOP WOOP */}
-    <div className="row">
-{/*                 
-                <div className="studieBarCol col"><button className="button">Videor</button></div>
-                <div className="studieBarCol col"><button className="button">Produktivitet</button></div>
-                <div className="studieBarCol col"><button className="button">Appar</button></div>
-                <div className="studieBarCol col"><button className="button">Hemsidor</button></div> */}
-                <div className=""><VideoContent/>
-                <ProdContent/></div>
-
-                    <div className="container"> 
-                        <div className="row">
-                            <div className="popup">
-                                <div classname="popup-inner">
-                                WITH THE WIND
-                                
-        
-          <PopupProd/>
-                                <button classname="button" onClick=""></button>
-                                </div>
-                            </div>
-                        </div> 
-                    </div>
-    </div>
-                    
-                        <div className="VideoDescText col"><h3>Lorem Ipsum</h3><p>Väldigt informativ video om Lorem Ipsum</p>
-                        </div> 
-                    <div> Elin starta här </div>
+ 
                 
                     <div> VIDEO </div>
                           
-                        <a href="https://www.youtube.com/watch?v=pZgMpjjgCRA">
-                            <div class="link">
+                        <a class="studie" href="https://www.youtube.com/watch?v=pZgMpjjgCRA">
+                            <div>
                                 Improving Cornell Notes With Sketchnoting Techniques
+                            </div>
+                        </a>
+                        <a class="studie" href="https://www.youtube.com/watch?v=IlU-zDU6aQ0&t">
+                            <div>
+                                Study Less Study Smart
                             </div>
                         </a>
                     
                     <div> HEMSIDA </div>
+                        <a class="studie" href="https://collegeinfogeek.com/">
+                            <div>
+                                College info geek
+                            </div>
+                        </a>
+                        <a class="studie" href="https://www.uu.se/en/students/support-and-services/health-care/study-techniques/">
+                            <div>
+                                Find the study technique that works best for you
+                            </div>
+                        </a>
                 
+                        <a class="studie" href="https://www.oxfordlearning.com/how-to-study-effectively/">
+                            <div>
+                                How to study effectively
+                            </div>
+                        </a>
                 
                     <div> BÖCKER </div>
+                        <a class="studie" href="https://www.adlibris.com/se/bok/plugga-smart-och-lar-dig-mer-9789144017259?gclid=Cj0KCQiA3b3gBRDAARIsAL6D-N8Ix2pjm2oJTAL1C8oHhxy5TCW8TR1fnTqlMM7voWIU3rfA-0WF4pgaAs9mEALw_wcB">
+                            <div>
+                               Plugga smart och lär dig mer!
+                            </div>
+                        </a>
+                        <a class="studie" href="https://www.bokus.com/bok/9780812981605/power-of-habit/">
+                            <div>
+                                Power Of Habit
+                            </div>
+                        </a>
+                    <div> APPAR </div>
+                        <a class="studie" href="https://www.forestapp.cc/en/">
+                            <div>
+                                Forest: Stay focused
+                            </div>
+                        </a>
                     
                     </div>
                 </div>

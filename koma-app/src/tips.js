0c3504f4ea5c75e9fbbb8696bf7922df463278cc
Azudator <lindqvist004@gmail.com>
2018-12-12 16:22:27 +0100

    Update koma-app/src/tips.js

--- koma-app/src/tips.js
+++ koma-app/src/tips.js
@@ -1,258 +1,129 @@
 import React, { Component } from 'react';
+
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import ReactDOM from 'react-dom';
 import './main.css';
 
-
 const TipsPage = () => ( 
-    <div><Tips/></div>
+ <div className="containWithStuff">
+        <div className=" row" id="root">
+</div>
+<App/>
+
+
+</div>
+
 );
 
-class PopupVideo extends React.Component{
+class App extends Component {
+        constructor() {
+            super();
+            this.handleClick1 = this.handleClick1.bind(this);
+            this.state = {
+                    
+                viewContent1: false,
+                viewContent2: false,
+                viewContent3: false,
+                viewContent4: false
+                // 0 could be your default view
+            }
+        }
     
-    // Initial states of Popup:
-    // Nollan counter, inga textfält (goalFields)
-    state = {
-        counter: 0,
-        enableButton: true,
-        goalFields: []
-    };
-    
-    // Arrow function
-    addGoal = () => {
-        // Plussa på countern
-        this.setState({ 
-            counter: this.state.counter +  1,
-            enableButton: false
-        });
-        // Kalla på createField
-        this.createField(this);
-        
-    } 
-    
-    render() {
-    {/**/}
-        return (
-          <div className='popup'>
-            <div className='popup_inner'>
+        handleClick1 = () => {
+            this.setState({ viewContent1: true, viewContent2: false, viewContent3: false, viewContent4: false })
+        }
 
-                 {/* CONTAINMENT WITH VIDEOS AND STUFF */}
+        handleClick2 = (button) => {
+                this.setState({viewContent1: false, viewContent2: true, viewContent3: false, viewContent4: false })
+            }
+
+        handleClick3 = (button) => {
+                this.setState({viewContent1: false, viewContent2: false, viewContent3: true, viewContent4: false })
+            }
+
+        handleClick4 = (button) => {
+                this.setState({ viewContent1: false, viewContent2: false, viewContent3: false, viewContent4: true  })
+            }
+        
+    
+        render() {
+            const click1= this.state.viewContent1;
+            const click2= this.state.viewContent2;
+            const click3= this.state.viewContent3;
+            const click4= this.state.viewContent4;
+            if(click1) return  (<div className="container" id="">
+            <div className="col-xl-11 self-align-center"><div className="row">
+            <button className='button button1 col' onClick={this.handleClick1}>VIDEO</button>
+                    <button className='button button2 col' onClick={this.handleClick2}> HEMSIDA</button>
+                    <button className='button button3 col' onClick={this.handleClick3}> BÖCKER</button>
+                    <button className='button button3 col' onClick={this.handleClick4}> APPAR</button>
+            <br/>
+            </div></div></div>)
+                else if (click2) return ( 
+                        <div className="container" id="">
+        <div className="col-xl-11 self-align-center"><div className="row">
+        <button className='button button1 col' onClick={this.handleClick1}>VIDEO</button>
+                <button className='button button2 col' onClick={this.handleClick2}> HEMSIDA</button>
+                <button className='button button3 col' onClick={this.handleClick3}> BÖCKER</button>
+                <button className='button button3 col' onClick={this.handleClick4}> APPAR</button>
+        <br/>
+        </div></div></div>
+                )
+                  else if (click3) return (<div className="container" id="">
+                  <div className="col-xl-11 self-align-center"><div className="row">
+                  <button className='button button1 col' onClick={this.handleClick1}>VIDEO</button>
+                          <button className='button button2 col' onClick={this.handleClick2}> HEMSIDA</button>
+                          <button className='button button3 col' onClick={this.handleClick3}> BÖCKER</button>
+                          <button className='button button3 col' onClick={this.handleClick4}> APPAR</button>
+                  <br/>
+                  </div></div></div>)
+                  else if (click4) return (
+                        <div className="container" id="">
+                        <div className="col-xl-11 self-align-center"><div className="row">
+                        <button className='button button1 col' onClick={this.handleClick1}>VIDEO</button>
+                                <button className='button button2 col' onClick={this.handleClick2}> HEMSIDA</button>
+                                <button className='button button3 col' onClick={this.handleClick3}> BÖCKER</button>
+                                <button className='button button3 col' onClick={this.handleClick4}> APPAR</button>
+                        <br/>
+                        </div></div></div>
+                  )
+
+        else return (<div className="container boxContent" id="">
+        <div className="col-xl-11 self-align-center"><div className="row">
+        <button className='button button1 col' onClick={this.handleClick1}>VIDEO</button>
+                <button className='button button2 col' onClick={this.handleClick2}> HEMSIDA</button>
+                <button className='button button3 col' onClick={this.handleClick3}> BÖCKER</button>
+                <button className='button button3 col' onClick={this.handleClick4}> APPAR</button>
+        <br/>
+        </div></div>
+        <div className="container"><div className="row"><div className="col-xl-11 self-align-center Blue">
+        
+        <div className="videosContainer">
+        <div className="VideoBox row">  <div className="col">
+        <iframe title="fuckIt" className="WhatEverBro" width="426" height="240" src="https://www.youtube.com/embed/aVS4W7GZSq0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
+        <div className="VideoDescText col"><h3>Lorem Ipsum</h3><p>Väldigt informativ video om Lorem Ipsum</p></div> </div>
+    </div> <hr/>
+
     <div className="videosContainer">
         <div className="VideoBox row">  <div className="col">
-        <iframe title="fuckIt" className="WhatEverBro" width="560" height="315" src="https://www.youtube.com/embed/aVS4W7GZSq0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
+        <iframe title="fuckIt" className="WhatEverBro" width="426" height="240" src="https://www.youtube.com/embed/aVS4W7GZSq0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
         <div className="VideoDescText col"><h3>Lorem Ipsum</h3><p>Väldigt informativ video om Lorem Ipsum</p></div> </div>
-    </div>
+    </div> <hr/>
 
-                
-
-                <button 
-                onClick={this.props.closePopup}>KLAR</button>
-            
-            </div>
-          </div>
-
-        );
-  }
- 
-
-}
-
-class PopupProd extends React.Component{
-    
-    // Initial states of Popup:
-    // Nollan counter, inga textfält (goalFields)
-    state = {
-        counter: 0,
-        enableButton: true,
-        goalFields: []
-    };
-    
-    // Arrow function
-    addGoal = () => {
-        // Plussa på countern
-        this.setState({ 
-            counter: this.state.counter +  1,
-            enableButton: false
-        });
-        // Kalla på createField
-        this.createField(this);
-        
-    } 
-    
-    render() {
-    {/**/}
-        return (
-          <div className='popup'>
-            <div className='popup_inner'>
-
-                 {/* CONTAINMENT WITH VIDEOS AND STUFF */}
     <div className="videosContainer">
-        ROLL THE DICE
-    </div>
+        <div className="VideoBox row">  <div className="col">
+        <iframe title="fuckIt" className="WhatEverBro" width="426" height="240" src="https://www.youtube.com/embed/aVS4W7GZSq0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
+        <div className="VideoDescText col"><h3>Lorem Ipsum</h3><p>Väldigt informativ video om Lorem Ipsum</p></div> </div>
+    </div> <hr/>
 
-                
-
-                <button 
-                onClick={this.props.closePopup}>KLAR</button>
-            
-            </div>
-          </div>
-
-        );
-  }
- 
-
-}
-
-
-class VideoContent extends React.Component {
     
-  constructor() {
-    super();
-    this.state = {
-      showPopup: false
-    };
-  }
     
-  togglePopup() {
-    this.setState({
-      showPopup: !this.state.showPopup
-    });
-  }
-    
-  render() {
-    return (
-      <div className="col">
-        <button onClick={this.togglePopup.bind(this)} className="button">
-            En knapp
-        </button>
-        
-        {/* Se detta som en if-sats:
-        // om showPopup == true, gör det som syns: 
-        // Annars, gör ingenting (null)*/}
-        {this.state.showPopup ? 
-          <PopupVideo
-            closePopup={this.togglePopup.bind(this)} 
-          />
-          : null
-        }
-
-      </div>
-    );
-  }
-}
-
-
-class ProdContent extends React.Component {
-    
-  constructor() {
-    super();
-    this.state = {
-      showPopup: false
-    };
-  }
-    
-  togglePopup() {
-    this.setState({
-      showPopup: !this.state.showPopup
-    });
-  }
-    
-  render() {
-    return (
-      <div className="col">
-        <button onClick={this.togglePopup.bind(this)} className="button">
-            Två knapp
-        </button>
-        
-        {/* Se detta som en if-sats:
-        // om showPopup == true, gör det som syns: 
-        // Annars, gör ingenting (null)*/}
-        {this.state.showPopup ? 
-          <PopupProd
-            closePopup={this.togglePopup.bind(this)} 
-          />
-          : null
-        }
-
-      </div>
-    );
-  }
-}
-
-class Tips extends React.Component{
-    
-    render(){
-        return ( <div>
-
- <div className="containWithStuff"> <div className="Blue col-s-9 offset-s-3">
- <h1 className="page-title"> Studieteknik? Ja, tack. </h1>
-
- <div className="studieBar col-s-10 offset-s-2"><p itemID="tipsText"> Har du svårt att hålla motivationen? Börjar du procrastinera så fort du får chansen?
-     No worries. Vi har samlat lite tips och tricks på studieteknik, som kanske kan hjälpa dig hålla fokus. 
-     
- </p>
- 
-                
-                    <div> VIDEO </div>
-                          
-                        <a class="studie" href="https://www.youtube.com/watch?v=pZgMpjjgCRA">
-                            <div>
-                                Improving Cornell Notes With Sketchnoting Techniques
-                            </div>
-                        </a>
-                        <a class="studie" href="https://www.youtube.com/watch?v=IlU-zDU6aQ0&t">
-                            <div>
-                                Study Less Study Smart
-                            </div>
-                        </a>
-                    
-                    <div> HEMSIDA </div>
-                        <a class="studie" href="https://collegeinfogeek.com/">
-                            <div>
-                                College info geek
-                            </div>
-                        </a>
-                        <a class="studie" href="https://www.uu.se/en/students/support-and-services/health-care/study-techniques/">
-                            <div>
-                                Find the study technique that works best for you
-                            </div>
-                        </a>
-                
-                        <a class="studie" href="https://www.oxfordlearning.com/how-to-study-effectively/">
-                            <div>
-                                How to study effectively
-                            </div>
-                        </a>
-                
-                    <div> BÖCKER </div>
-                        <a class="studie" href="https://www.adlibris.com/se/bok/plugga-smart-och-lar-dig-mer-9789144017259?gclid=Cj0KCQiA3b3gBRDAARIsAL6D-N8Ix2pjm2oJTAL1C8oHhxy5TCW8TR1fnTqlMM7voWIU3rfA-0WF4pgaAs9mEALw_wcB">
-                            <div>
-                               Plugga smart och lär dig mer!
-                            </div>
-                        </a>
-                        <a class="studie" href="https://www.bokus.com/bok/9780812981605/power-of-habit/">
-                            <div>
-                                Power Of Habit
-                            </div>
-                        </a>
-                    <div> APPAR </div>
-                        <a class="studie" href="https://www.forestapp.cc/en/">
-                            <div>
-                                Forest: Stay focused
-                            </div>
-                        </a>
-                    
-                    </div>
-                </div>
-
-            </div>
-
-        </div>
-       
-
-            
+    </div></div></div></div>
         )
-    }
+        }
+
+        
 }
+ReactDOM.render(<App />, document.getElementById('root'));
 
 export default TipsPage;

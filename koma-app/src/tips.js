55256837975ee0b36f7c9bcf9779ee5d36151636
Azudator <lindqvist004@gmail.com>
2018-12-14 11:17:59 +0100

    WOOH

--- koma-app/src/tips.js
+++ koma-app/src/tips.js
@@ -81,35 +81,76 @@ class App extends Component {
         </div> <hr/>
     
         
-        
+        {/* HEMSIDA */}
         </div></div></div></div>)
-                else if (click2) return ( 
-                        <div className="container" id="">
-        <div className="col-xl-11 self-align-center"><div className="row">
-        <button className='button button1 col' onClick={this.handleClick1}>VIDEO</button>
-                <button className='button button2 col' onClick={this.handleClick2}> HEMSIDA</button>
-                <button className='button button3 col' onClick={this.handleClick3}> BÖCKER</button>
-                <button className='button button3 col' onClick={this.handleClick4}> APPAR</button>
-        <br/>
-        </div></div></div>
+                else if (click2) return ( <div className="container boxContent" id="">
+                <div className="col-xl-11 self-align-center"><div className="row">
+                <button className='button button1 col' onClick={this.handleClick1}>VIDEO</button>
+                        <button className='button button2 col' onClick={this.handleClick2}> HEMSIDA</button>
+                        <button className='button button3 col' onClick={this.handleClick3}> BÖCKER</button>
+                        <button className='button button3 col' onClick={this.handleClick4}> APPAR</button>
+                <br/>
+                </div></div>
+                <div className="container"><div className="row"><div className="col-xl-11 self-align-center Blue">
+                
+                <div className="videosContainer">
+                <div className="VideoBox row">  <div className="col">
+                <img src="https://i.imgur.com/mxlkm4T.png" className="WhatEverBrosephine" alt="uppsala university"></img></div>
+                <div className="VideoDescText col"><h3>Find the study technique that works best for you</h3><p>Starting university can be present a major change for many - with new challenges and new environments. It is important to adapt as quickly as possible in order to keep up. University studies often involve a relative freedom for students. This places greater demands on you to take more responsibility for your own learning. Many forms of instruction are new and the number of students in some programmes can be large.</p></div> </div>
+            </div> <hr/>
+        
+            <a href="https://www.oxfordlearning.com/how-to-study-effectively/"><div className="videosContainer">
+                <div className="VideoBox row">  <div className="col">
+                <img src="https://i.imgur.com/V0jo4Xj.jpg" className="WhatEverBrosephine" alt="uppsala university"></img></div>
+                <div className="VideoDescText col"><h3>How to study effectively</h3><p>Being properly organized and prepared for tests and exams can make all the difference to school performance. Effective studying starts with the right attitude—a positive outlook can shift studying from a punishment to an opportunity to learn.</p></div> </div>
+            </div></a> <hr/>
+        
+        
+            
+            
+            </div></div></div></div>
                 )
-                  else if (click3) return (<div className="container" id="">
+
+
+
+                  else if (click3) return (<div className="container boxContent" id="">
                   <div className="col-xl-11 self-align-center"><div className="row">
                   <button className='button button1 col' onClick={this.handleClick1}>VIDEO</button>
                           <button className='button button2 col' onClick={this.handleClick2}> HEMSIDA</button>
                           <button className='button button3 col' onClick={this.handleClick3}> BÖCKER</button>
                           <button className='button button3 col' onClick={this.handleClick4}> APPAR</button>
                   <br/>
-                  </div></div></div>)
+                  </div></div>
+                  <div className="container"><div className="row"><div className="col-xl-11 self-align-center Blue">
+                  
+                  <div className="videosContainer" >
+              <div className="VideoBox row">  <div className="col">
+              <img src="https://s1.adlibris.com/images/279390/plugga-smart-och-lar-dig-mer.jpg" className="WhatEverBrosephine" alt="uppsala university"></img></div>
+              <div className="VideoDescText col"><h3>Plugga smart och lär dig mer!</h3><p>Being properly organized and prepared for tests and exams can make all the difference to school performance. Effective studying starts with the right attitude—a positive outlook can shift studying from a punishment to an opportunity to learn.</p></div> </div>
+          </div> <hr/>
+              
+              
+              </div></div></div></div>)
                   else if (click4) return (
-                        <div className="container" id="">
-                        <div className="col-xl-11 self-align-center"><div className="row">
-                        <button className='button button1 col' onClick={this.handleClick1}>VIDEO</button>
-                                <button className='button button2 col' onClick={this.handleClick2}> HEMSIDA</button>
-                                <button className='button button3 col' onClick={this.handleClick3}> BÖCKER</button>
-                                <button className='button button3 col' onClick={this.handleClick4}> APPAR</button>
-                        <br/>
-                        </div></div></div>
+                    <div className="container boxContent" id="">
+                    <div className="col-xl-11 self-align-center"><div className="row">
+                    <button className='button button1 col' onClick={this.handleClick1}>VIDEO</button>
+                            <button className='button button2 col' onClick={this.handleClick2}> HEMSIDA</button>
+                            <button className='button button3 col' onClick={this.handleClick3}> BÖCKER</button>
+                            <button className='button button3 col' onClick={this.handleClick4}> APPAR</button>
+                    <br/>
+                    </div></div>
+                    <div className="container"><div className="row"><div className="col-xl-11 self-align-center Blue">
+                    
+                    <a href=""><div className="videosContainer">
+                <div className="VideoBox row">  <div className="col">
+                <img src="https://www.forestapp.cc/img/icon.png" className="WhatEverBrosephine" alt="uppsala university"></img></div>
+                <div className="VideoDescText col"><h3>Forest Focus App</h3><p>Build Your Forest
+Keep building your forest everyday, every single tree represents your focused time</p></div> </div>
+            </div></a> <hr/>
+                
+                
+                </div></div></div></div>
                   )
 
         else return (<div className="container boxContent" id="">
@@ -122,24 +163,9 @@ class App extends Component {
         </div></div>
         <div className="container"><div className="row"><div className="col-xl-11 self-align-center Blue">
         
-        <div className="videosContainer">
-        <div className="VideoBox row">  <div className="col">
-        <iframe title="fuckIt" className="WhatEverBro" width="426" height="240" src="https://www.youtube.com/embed/aVS4W7GZSq0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
-        <div className="VideoDescText col"><h3>Lorem Ipsum</h3><p>Väldigt informativ video om Lorem Ipsum</p></div> </div>
-    </div> <hr/>
-
-    <div className="videosContainer">
-        <div className="VideoBox row">  <div className="col">
-        <iframe title="fuckIt" className="WhatEverBro" width="426" height="240" src="https://www.youtube.com/embed/aVS4W7GZSq0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
-        <div className="VideoDescText col"><h3>Lorem Ipsum</h3><p>Väldigt informativ video om Lorem Ipsum</p></div> </div>
-    </div> <hr/>
-
-    <div className="videosContainer">
-        <div className="VideoBox row">  <div className="col">
-        <iframe title="fuckIt" className="WhatEverBro" width="426" height="240" src="https://www.youtube.com/embed/aVS4W7GZSq0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
-        <div className="VideoDescText col"><h3>Lorem Ipsum</h3><p>Väldigt informativ video om Lorem Ipsum</p></div> </div>
-    </div> <hr/>
-
+        <div className="VideoBox row">
+                <div className="VideoDescText col"><h3>Studieteknik</h3><p>Lite studietips! Här kan du få tips och tekniker för dig att plugga och studera vettigt, smart och EFFEKTIVT! Tryck ovan på de fyra fina knapparna.</p></div> </div>
+            <hr/>
     
     
     </div></div></div></div>
